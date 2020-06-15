8ac76903036d76b68125bc7cfb54a1582f317b42
Jorg Nieberg <jorg.nieberg@kabisa.nl>
2020-06-15 11:14:26 +0200

    Fixed indent. In Font NodeJS, we now use the same CSS format function from Font.js

--- Font.js
+++ Font.js
@@ -13,7 +13,7 @@ import { _window } from "./lib/window.js";
  *
  * @param {*} path
  */
-function getFontCSSFormat(path) {
+export function getFontCSSFormat(path) {
     let pos = path.lastIndexOf(`.`);
     let ext = (path.substring(pos + 1) || ``).toLowerCase();
     let format = {
@@ -22,19 +22,21 @@ function getFontCSSFormat(path) {
         woff: `woff`,
         woff2: `woff2`
     }[ext];
-
+    
     if (format) return format;
 
-    let msg = {
-        eot: `The .eot format is not supported: it died in January 12, 2016, when Microsoft retired all versions of IE that didn't already support WOFF.`,
-        svg: `The .svg format is not supported: SVG fonts (not to be confused with OpenType with embedded SVG) were so bad we took the entire fonts chapter out of the SVG specification again.`,
-        fon: `The .fon format is not supported: this is an ancient Windows bitmap font format.`,
-        ttc: `Based on the current CSS specification, font collections are not (yet?) supported.`
-    }[ext];
+    if(_window) {
+        let msg = {
+            eot: `The .eot format is not supported: it died in January 12, 2016, when Microsoft retired all versions of IE that didn't already support WOFF.`,
+            svg: `The .svg format is not supported: SVG fonts (not to be confused with OpenType with embedded SVG) were so bad we took the entire fonts chapter out of the SVG specification again.`,
+            fon: `The .fon format is not supported: this is an ancient Windows bitmap font format.`,
+            ttc: `Based on the current CSS specification, font collections are not (yet?) supported.`
+        }[ext];
 
-    if (!msg) msg = `${url} is not a font.`;
+        if (!msg) msg = `${url} is not a font.`;
 
-    this.dispatch(new Event(`error`, {}, msg));
+        this.dispatch(new Event(`error`, {}, msg));
+    }
 }
 
 
