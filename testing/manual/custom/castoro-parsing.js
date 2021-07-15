a3112bb6cc65cf8daaf8e6f1376fae51bd69b698
Pomax <pomax@nihongoresources.com>
2021-07-15 11:02:04 -0700

    .

---
+++ testing/manual/custom/castoro-parsing.js
@@ -0,0 +1,48 @@
+import { Font } from "../../../lib-font.js";
+
+const font = new Font("castoro");
+font.onload = (evt) => {
+  try {
+    testFont(evt.detail.font);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+font.src = "./fonts/issue-123/Castoro-Regular.woff2";
+
+function testFont(font) {
+  const { GSUB } = font.opentype.tables;
+  const scripts = GSUB.getSupportedScripts();
+  let allGlyphs = {};
+
+  scripts.forEach((script) => {
+    let langsys = GSUB.getSupportedLangSys(script);
+
+    allGlyphs[script] = {};
+
+    langsys.forEach((lang) => {
+      let langSysTable = GSUB.getLangSysTable(script, lang);
+      let features = GSUB.getFeatures(langSysTable);
+
+      allGlyphs[script][lang] = {};
+
+      features.forEach((feature) => {
+        const lookupIDs = feature.lookupListIndices;
+        allGlyphs[script][lang][feature.featureTag] = {};
+        allGlyphs[script][lang][feature.featureTag]["lookups"] = [];
+
+        lookupIDs.forEach((id) => {
+          const lookup = GSUB.getLookup(id);
+
+          lookup.subtableOffsets.forEach((_, i) => {
+            console.log(
+              `Getting subtable ${i}, lookup ${id}, feature ${feature.featureTag}, script ${script} , lang ${lang}`
+            );
+            const subtable = lookup.getSubTable(i);
+          });
+        });
+      });
+    });
+  });
+}
