5f410846f4840e0c054377c241c79276b83e8b21
Pomax <pomax@nihongoresources.com>
2021-07-15 11:33:15 -0700

    add type to GPOS builder

--- src/opentype/tables/advanced/shared/subtables/gpos.js
+++ src/opentype/tables/advanced/shared/subtables/gpos.js
@@ -10,25 +10,19 @@ import { LookupType9 } from "../../lookups/gpos/lookup-type-9.js";
 
 export default {
   buildSubtable: function (type, p) {
-    switch (type) {
-      case 1:
-        return new LookupType1(p);
-      case 2:
-        return new LookupType2(p);
-      case 3:
-        return new LookupType3(p);
-      case 4:
-        return new LookupType4(p);
-      case 5:
-        return new LookupType5(p);
-      case 6:
-        return new LookupType6(p);
-      case 7:
-        return new LookupType7(p);
-      case 8:
-        return new LookupType8(p);
-      case 9:
-        return new LookupType9(p);
-    }
+    const subtable = new [
+      undefined,
+      LookupType1,
+      LookupType2,
+      LookupType3,
+      LookupType4,
+      LookupType5,
+      LookupType6,
+      LookupType7,
+      LookupType8,
+      LookupType9,
+    ][type](p);
+    subtable.type = type;
+    return subtable;
   },
 };
