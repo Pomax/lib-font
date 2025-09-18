4b92419fe9d3e125b83ca0d68b251576f0292e97
Robert Janes <robertjanes2@gmail.com>
2025-09-18 17:02:18 +0200

    Added more cmap tests

--- testing/node/cmap.test.js
+++ testing/node/cmap.test.js
@@ -12,11 +12,28 @@ async function main() {
     path.join(__dirname, "../../fonts/Inter-Regular.otf"),
     "Inter Regular"
   );
-
   assert(font.opentype.tables.cmap.getGlyphId(65) === 2); // 0x0041
   assert(font.opentype.tables.cmap.getGlyphId(61153) === 2932); // 0xeee1
   assert(font.opentype.tables.cmap.getGlyphId(129106) === 1807); // 0x1f852
   assert(font.opentype.tables.cmap.getGlyphId(61087) === 2913); // 0xee9f
+
+  const font2 = await loadFontFromPath(
+    path.join(__dirname, "../../fonts/SourceCodePro/SourceCodePro-Regular.ttf"),
+    "Source Code Pro"
+  );
+  assert(font2.opentype.tables.cmap.getGlyphId(65) === 2);
+  assert(font2.opentype.tables.cmap.getGlyphId(0x1f3b6) === 1560);
+  assert(font2.opentype.tables.cmap.getGlyphId(0x2500) === 1395);
+  assert(font2.opentype.tables.cmap.getGlyphId(0xfb02) === 1577);
+
+  const font3 = await loadFontFromPath(
+    path.join(__dirname, "../../fonts/Recursive_VF_1.064.ttf"),
+    "Recursive"
+  );
+  assert(font3.opentype.tables.cmap.getGlyphId(65) === 7);
+  assert(font3.opentype.tables.cmap.getGlyphId(0x00ad) === 716);
+  assert(font3.opentype.tables.cmap.getGlyphId(0x25bc) === 704);
+  assert(font3.opentype.tables.cmap.getGlyphId(0x00b5) === 1100);
 }
 
 async function loadFontFromPath(path, name = "Untitled Font") {
